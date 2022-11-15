00a64b6dc1f454721cb1ee109746e77b71136718
Dima Povkh <dimapovkh@gmail.com>
2022-11-15 12:01:25 +0100

    added class Meta and def __str__() in model Cook

--- menu/models.py
+++ menu/models.py
@@ -16,6 +16,13 @@ class DishType(models.Model):
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(max_length=63, null=True, blank=True)
 
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
 
 class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
