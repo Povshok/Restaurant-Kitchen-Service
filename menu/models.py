b4928bd2d3af29c307b24b8c705f113845c9cda2
Dima Povkh <dimapovkh@gmail.com>
2022-11-15 11:48:36 +0100

    Created model Dish. Added field with ForeignKey to model DishType and field with ManyToManyField to model Cook.

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,3 +10,17 @@ class DishType(models.Model):
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(max_length=63, null=True, blank=True)
 
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="cooks"
+    )
