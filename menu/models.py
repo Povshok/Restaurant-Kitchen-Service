ca5a4b5559820d83cf9a18182926bddda8fdf540
Dima Povkh <dimapovkh@gmail.com>
2022-11-15 11:32:34 +0100

    Added model Cook and import AbstractUser

--- menu/models.py
+++ menu/models.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(max_length=63, null=True, blank=True)
